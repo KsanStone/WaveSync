8a6263bc6585bf8208b83e3e1fcdaa3a5ea40f9c
KsanStone <filip.krusen@gmail.com>
2024-02-09 20:47:29 +0100

    Peak val

--- src/main/kotlin/me/ksanstone/wavesync/wavesync/gui/controller/FFTInfoController.kt
+++ src/main/kotlin/me/ksanstone/wavesync/wavesync/gui/controller/FFTInfoController.kt
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable
 import javafx.scene.control.Label
 import me.ksanstone.wavesync.wavesync.WaveSyncBootApplication
 import me.ksanstone.wavesync.wavesync.service.AudioCaptureService
+import me.ksanstone.wavesync.wavesync.service.fftScaling.DeciBelFFTScalar
 import java.net.URL
 import java.text.DecimalFormat
 import java.util.*
@@ -13,12 +14,17 @@ import java.util.*
 class FFTInfoController : Initializable{
 
     @FXML
+    lateinit var peakVLabel: Label
+
+    @FXML
     lateinit var peakLabel: Label
 
     private val acs = WaveSyncBootApplication.applicationContext.getBean(AudioCaptureService::class.java)
     private val peakFormat = DecimalFormat("###.##")
+    private val deciBelFFTScalar = DeciBelFFTScalar()
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         acs.peakFrequency.addListener { _, _, v -> Platform.runLater { peakLabel.text = peakFormat.format(v) + " Hz" }}
+        acs.peakValue.addListener { _, _, v -> Platform.runLater { peakVLabel.text = peakFormat.format(deciBelFFTScalar.scaleRaw(v.toFloat())) + " dB" }}
     }
 }
